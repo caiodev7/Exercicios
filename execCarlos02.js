c22dec6915e8932b730eaa2371f5c3b2dc622c34
Caio miguel <164591812+caiodev7@users.noreply.github.com>
2024-08-10 12:41:37 -0300

    Update execCarlos02.js

--- execCarlos02.js
+++ execCarlos02.js
@@ -1,11 +1,11 @@
     let horabrasil, horafranca
   
-    horabrasil = parseFloat(prompt('Que horas são aqui no Brasil?'));
-    horafranca = horabrasil + 5;
+    horabrasil = parseFloat(prompt('Que horas são aqui no Brasil?'))
+    horafranca = horabrasil + 5
 
     //Condição para ajustar a hora na frança para o formato de 24 horas
     if (horafranca >= 24) {
-     horafranca -= 24; 
+     horafranca -= 24
     }
     
-    alert(`A hora aqui no Brasil é: ${horabrasil.toFixed(2)} e na França é: ${horafranca.toFixed(2)}`);
+    alert(`A hora aqui no Brasil é: ${horabrasil.toFixed(2)} e na França é: ${horafranca.toFixed(2)}`)
